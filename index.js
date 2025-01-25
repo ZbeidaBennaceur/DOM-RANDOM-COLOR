85165b30be5e729a0efbd7d503a0ef926f36d9b5
ZbeidaBennaceur <zbeidabennaceur@gmail.com>
2025-01-25 10:04:06 +0100

    third commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 document.addEventListener("DOMContentLoaded",function() {
 const square = document.querySelector("#color-box");
 const button= document.querySelector("#change-color-btn");
+const titre=document.querySelector(".titre");
+
 
 button.addEventListener("click",function getRandomColor (){
     let R=Math.floor(Math.random()*255);
@@ -10,5 +12,6 @@ button.addEventListener("click",function getRandomColor (){
 
     square.style.backgroundColor=`rgb(${R},${V},${B})`;
     button.style.backgroundColor=`rgb(${R},${V},${B})`;
+    titre.style.color=`rgb(${R},${V},${B})`;
 });
 })
